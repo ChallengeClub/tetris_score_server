ba97bf375fbe5c95aa4c10ad867a95cf888c5a44
EndoNrak <spheniscushumboldtievangelion@gmail.com>
2022-08-12 21:10:31 +0900

    fix env variables

--- server/server/settings.py
+++ server/server/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from .secret_setting import SECRET_KEY
+import os
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

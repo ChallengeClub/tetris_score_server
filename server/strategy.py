b75e00b7d008e29ab2d2196b8448370d885b96aa
EndoNrak <spheniscushumboldtievangelion@gmail.com>
2022-08-13 22:47:52 +0900

    change .env

--- server/strategy.py
+++ server/strategy.py
@@ -20,13 +20,14 @@ def clone_repository(url: str, branch: str):
 def tetris_start(level=1, game_time=180, drop_interval=1000, value_mode="sample", value_predict_weight=""):
     os.chdir("/home/tetris")
     os.environ["QT_QPA_PLATFORM"]="offscreen"
-    print(os.environ)
     if value_predict_weight == "":
-        tetris_start_command = f"python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode}"
+        tetris_start_command = f"env \\n python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode}"
     else:
-        tetris_start_command = f"python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode} --predict_weight {value_predict_weight}"
-    subprocess.run(tetris_start_command.split(), capture_output=True, encoding='utf-8')
-    
+        tetris_start_command = f"env \\n python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode} --predict_weight {value_predict_weight}"
+    # tetris_start_command = "QT_QPA_PLATFORM=offscreen && " + tetris_start_command
+
+    result = subprocess.run(tetris_start_command.split(), capture_output=True, encoding='utf-8')
+    print(result)
 
 if __name__=="__main__":
     clone_repository(url="https://github.com/seigot/tetris", branch="master")

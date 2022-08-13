f1cae89009af8b4b9f044d866d2672e4f9ae0b02
EndoNrak <spheniscushumboldtievangelion@gmail.com>
2022-08-13 22:21:30 +0900

    create strategy.py

---
+++ server/strategy.py
@@ -0,0 +1,33 @@
+from curses import resetty
+import os
+import shutil
+import subprocess
+from urllib import response
+
+def clone_repository(url: str, branch: str):
+    """
+    clone repository in /home/tetris
+    if tetris folder is already exists, git clone after removing
+
+    """
+    os.chdir("/home")
+    if os.path.exists("tetris"):
+        shutil.rmtree("tetris")
+    git_clone_command = f"git clone {url} -b {branch} tetris --depth=1" # --depth=1: clone only head
+    result = subprocess.run(git_clone_command.split(), capture_output=True, encoding='utf-8')
+
+
+def tetris_start(level=1, game_time=180, drop_interval=1000, value_mode="sample", value_predict_weight=""):
+    os.chdir("/home/tetris")
+    os.environ["QT_QPA_PLATFORM"]="offscreen"
+    print(os.environ)
+    if value_predict_weight == "":
+        tetris_start_command = f"python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode}"
+    else:
+        tetris_start_command = f"python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode} --predict_weight {value_predict_weight}"
+    subprocess.run(tetris_start_command.split(), capture_output=True, encoding='utf-8')
+    
+
+if __name__=="__main__":
+    clone_repository(url="https://github.com/seigot/tetris", branch="master")
+    tetris_start()

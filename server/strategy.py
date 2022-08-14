08c6547a51127036fdcd38d8edef8fd55cfe8a94
EndoNrak <spheniscushumboldtievangelion@gmail.com>
2022-08-15 00:26:40 +0900

    fix tetris_start func

--- server/strategy.py
+++ server/strategy.py
@@ -17,10 +17,9 @@ def clone_repository(url: str, branch: str):
 
 def tetris_start(level=1, game_time=180, drop_interval=1000, value_mode="default", value_predict_weight="", log_file="result.json"):
     os.chdir("/home/tetris")
-    if value_predict_weight == "":
-        tetris_start_command = f"xvfb-run -a python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode} -f {log_file}"
-    else:
-        tetris_start_command = f"xvfb-run -a python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode} -f {log_file} --predict_weight {value_predict_weight}"
+    tetris_start_command = f"xvfb-run -a python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode} -f {log_file}"
+    if value_predict_weight != "":
+        tetris_start_command += f" --predict_weight {value_predict_weight}"
     result = subprocess.run(tetris_start_command.split(), capture_output=True, encoding='utf-8')
     return result
 

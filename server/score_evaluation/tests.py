30d3ea02869fae42e6ba2bb7983538cd046bf329
EndoNrak <spheniscushumboldtievangelion@gmail.com>
2022-08-15 23:41:35 +0900

    fix test

--- server/score_evaluation/tests.py
+++ server/score_evaluation/tests.py
@@ -6,7 +6,7 @@ class ScoreEvaluationTests(TestCase):
     def test_default_evaluation(self):
         result = strategy(
             url="https://github.com/seigot/tetris",
-            branch="masteree",
+            branch="master",
             repeet=5,
             level=1,
             game_time=10

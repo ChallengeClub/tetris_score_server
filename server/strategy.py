220b735d204abb55fcfc4f6de881dd62aefec6c3
EndoNrak <spheniscushumboldtievangelion@gmail.com>
2022-08-17 13:36:39 +0900

    fix strategy.py cleaned

--- server/strategy.py
+++ server/strategy.py
@@ -15,13 +15,12 @@ def clone_repository(url: str, branch: str):
     result = subprocess.run(git_clone_command.split(), capture_output=True, encoding='utf-8')
     return result
 
-def tetris_start(level=1, game_time=180, drop_interval=1000, value_mode="sample", value_predict_weight=""):
+def tetris_start(level=1, game_time=180, drop_interval=1000, value_mode="sample", value_predict_weight="", log_file="result.json"):
     os.chdir("/home/tetris")
-    os.environ["QT_QPA_PLATFORM"]="offscreen"
     if value_predict_weight == "":
-        tetris_start_command = f"xvfb-run -a python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode}"
+        tetris_start_command = f"xvfb-run -a python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode} -f {log_file}"
     else:
-        tetris_start_command = f"xvfb-run -a python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode} --predict_weight {value_predict_weight}"
+        tetris_start_command = f"xvfb-run -a python start.py -l {level} -t {game_time} -d {drop_interval} -m {value_mode} -f {log_file} --predict_weight {value_predict_weight}"
     result = subprocess.run(tetris_start_command.split(), capture_output=True, encoding='utf-8')
     return result
 
@@ -30,6 +29,7 @@ def strategy():
     branch = "master"
     repeet = 5
     game_time = 10
+    log_folder = "/server/log"
     res = clone_repository(url=url, branch=branch)
     if res.returncode:
         return res
@@ -38,10 +38,10 @@ def strategy():
     futures = []
     with ThreadPoolExecutor() as pool:
         for i in range(repeet):
-            future = pool.submit(tetris_start, game_time=game_time)
+            future = pool.submit(tetris_start, game_time=game_time, log_file=f"{log_folder}/result-{i}.json")
             futures.append(future)
     for i, future in enumerate(futures):
-        with open(f"/server/log/result-{i}.log", 'w', encoding='utf-8') as f:
+        with open(f"{log_folder}/result-{i}.log", 'w', encoding='utf-8') as f:
             f.write(future.result().stdout)
 
 

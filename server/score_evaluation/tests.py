166645e1a2210262773ffbb94656bac51e579828
EndoNrak <spheniscushumboldtievangelion@gmail.com>
2022-08-22 22:55:16 +0900

    fix tests.py

--- server/score_evaluation/tests.py
+++ server/score_evaluation/tests.py
@@ -8,65 +8,65 @@ from .infrastructure.sqs_infrastructure import EvaluationMessageRepositoryInterf
 from .model.models import Evaluation
 from .model.score_evaluation_message_pb2 import ScoreEvaluationMessage
 
-# class ScoreEvaluationTests(TestCase):
-#     def test_default_evaluation(self):
-#         eval = Evaluation(
-#             repository_url="https://github.com/seigot/tetris",
-#             branch="master",
-#             trial_num=5,
-#             level=1,
-#             game_time=2
-#         )
-#         usecase = ScoreEvaluationUsecase(eval)
-#         eval = usecase.evaluate()
-#         self.assertEqual(eval.status, "S")
+class ScoreEvaluationTests(TestCase):
+    def test_default_evaluation(self):
+        eval = Evaluation(
+            repository_url="https://github.com/seigot/tetris",
+            branch="master",
+            trial_num=5,
+            level=1,
+            game_time=2
+        )
+        usecase = ScoreEvaluationUsecase(eval)
+        eval = usecase.evaluate()
+        self.assertEqual(eval.status, "S")
 
-#     def test_error_branch(self):
-#         """
-#         with invalid branch name, git clone would be failed
-#         """   
-#         eval = Evaluation(
-#             repository_url="https://github.com/seigot/tetris",
-#             branch="masterrr",
-#             trial_num=1,
-#             level=1,
-#             game_time=2
-#         )
-#         usecase = ScoreEvaluationUsecase(eval)
-#         eval = usecase.evaluate()
-#         self.assertEqual(eval.status, "ER")
+    def test_error_branch(self):
+        """
+        with invalid branch name, git clone would be failed
+        """   
+        eval = Evaluation(
+            repository_url="https://github.com/seigot/tetris",
+            branch="masterrr",
+            trial_num=1,
+            level=1,
+            game_time=2
+        )
+        usecase = ScoreEvaluationUsecase(eval)
+        eval = usecase.evaluate()
+        self.assertEqual(eval.status, "ER")
     
-#     def test_error_empty_value_mode(self):
-#         """
-#         with brank value mode, tetris start cmd would be failed
-#         """  
-#         eval = Evaluation(
-#             repository_url="https://github.com/seigot/tetris",
-#             branch="master",
-#             game_mode = "",
-#             trial_num=1,
-#             level=1,
-#             game_time=2
-#         )
-#         usecase = ScoreEvaluationUsecase(eval)
-#         eval = usecase.evaluate()
-#         self.assertEqual(eval.status, "ER")
+    def test_error_empty_value_mode(self):
+        """
+        with brank value mode, tetris start cmd would be failed
+        """  
+        eval = Evaluation(
+            repository_url="https://github.com/seigot/tetris",
+            branch="master",
+            game_mode = "",
+            trial_num=1,
+            level=1,
+            game_time=2
+        )
+        usecase = ScoreEvaluationUsecase(eval)
+        eval = usecase.evaluate()
+        self.assertEqual(eval.status, "ER")
     
-#     def test_error_time_out(self):
-#         """
-#         with brank value mode, tetris start cmd would be failed
-#         """  
-#         eval = Evaluation(
-#             repository_url="https://github.com/seigot/tetris",
-#             branch="master",
-#             trial_num=1,
-#             level=1,
-#             game_time=10,
-#             timeout=1
-#         )
-#         usecase = ScoreEvaluationUsecase(eval)
-#         eval = usecase.evaluate()
-#         self.assertEqual(eval.status, "ER")
+    def test_error_time_out(self):
+        """
+        with brank value mode, tetris start cmd would be failed
+        """  
+        eval = Evaluation(
+            repository_url="https://github.com/seigot/tetris",
+            branch="master",
+            trial_num=1,
+            level=1,
+            game_time=10,
+            timeout=1
+        )
+        usecase = ScoreEvaluationUsecase(eval)
+        eval = usecase.evaluate()
+        self.assertEqual(eval.status, "ER")
         
 
 class InterfaceTests(TestCase):

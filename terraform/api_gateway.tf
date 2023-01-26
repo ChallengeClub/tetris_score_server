9c5de6c59c4ffc11bb407961c81c0e98dab5ae75
EndoNrak <endo.noriaki.64m@kyoto-u.jp>
2023-01-26 23:00:26 +0900

    add apigw

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,44 +1,44 @@
-# resource "aws_apigatewayv2_api" "tetris_api" {
-#   name          = var.api_gateway_name
-#   protocol_type = "HTTP"
-#   cors_configuration {
-#     allow_origins = var.api_gateway_allow_origins
-#   }
-# }
+resource "aws_apigatewayv2_api" "tetris_api" {
+  name          = var.api_gateway_name
+  protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = var.api_gateway_allow_origins
+  }
+}
 
-# resource "aws_apigatewayv2_stage" "tetris_api_stage" {
-#   api_id      = aws_apigatewayv2_api.tetris_api.id
-#   name        = var.api_gateway_stage_name
-#   auto_deploy = true
-#   route_settings {
-#     route_key              = "POST /score_evaluation"
-#     logging_level          = "ERROR"
-#     throttling_rate_limit  = 10
-#     throttling_burst_limit = 10
-#   }
-#   route_settings {
-#     route_key              = "GET /results"
-#     logging_level          = "ERROR"
-#     throttling_rate_limit  = 10
-#     throttling_burst_limit = 10
-#   }
-#   route_settings {
-#     route_key              = "POST /entry"
-#     logging_level          = "ERROR"
-#     throttling_rate_limit  = 10
-#     throttling_burst_limit = 10
-#   }
-#   route_settings {
-#     route_key              = "GET /entries"
-#     logging_level          = "ERROR"
-#     throttling_rate_limit  = 10
-#     throttling_burst_limit = 10
-#   }
-#   access_log_settings {
-#     destination_arn = aws_cloudwatch_log_group.apigateway_accesslog.arn
-#     format          = var.api_gateway_access_log_format
-#   }
-# }
+resource "aws_apigatewayv2_stage" "tetris_api_stage" {
+  api_id      = aws_apigatewayv2_api.tetris_api.id
+  name        = var.api_gateway_stage_name
+  auto_deploy = true
+  route_settings {
+    route_key              = "POST /score_evaluation"
+    logging_level          = "ERROR"
+    throttling_rate_limit  = 10
+    throttling_burst_limit = 10
+  }
+  route_settings {
+    route_key              = "GET /results"
+    logging_level          = "ERROR"
+    throttling_rate_limit  = 10
+    throttling_burst_limit = 10
+  }
+  route_settings {
+    route_key              = "POST /entry"
+    logging_level          = "ERROR"
+    throttling_rate_limit  = 10
+    throttling_burst_limit = 10
+  }
+  route_settings {
+    route_key              = "GET /entries"
+    logging_level          = "ERROR"
+    throttling_rate_limit  = 10
+    throttling_burst_limit = 10
+  }
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.apigateway_accesslog.arn
+    format          = var.api_gateway_access_log_format
+  }
+}
 
 # resource "aws_apigatewayv2_integration" "send_message_to_sqs_lambda_integration" {
 #   api_id                 = aws_apigatewayv2_api.tetris_api.id

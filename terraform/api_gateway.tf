b4d0a33815bc85407b80b6109dab84fd4873cbc4
EndoNrak <endo.noriaki.64m@kyoto-u.jp>
2023-01-26 22:54:23 +0900

    delete apigw lambda integrations

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -40,58 +40,58 @@ resource "aws_apigatewayv2_stage" "tetris_api_stage" {
   }
 }
 
-resource "aws_apigatewayv2_integration" "send_message_to_sqs_lambda_integration" {
-  api_id                 = aws_apigatewayv2_api.tetris_api.id
-  integration_uri        = aws_lambda_function.function.invoke_arn
-  integration_type       = "AWS_PROXY"
-  integration_method     = "POST"
-  payload_format_version = "2.0"
-}
+# resource "aws_apigatewayv2_integration" "send_message_to_sqs_lambda_integration" {
+#   api_id                 = aws_apigatewayv2_api.tetris_api.id
+#   integration_uri        = aws_lambda_function.function.invoke_arn
+#   integration_type       = "AWS_PROXY"
+#   integration_method     = "POST"
+#   payload_format_version = "2.0"
+# }
 
-resource "aws_apigatewayv2_integration" "get_results_from_dynamodb_lambda_integration" {
-  api_id                 = aws_apigatewayv2_api.tetris_api.id
-  integration_uri        = aws_lambda_function.get_result_from_dynamodb_function.invoke_arn
-  integration_type       = "AWS_PROXY"
-  integration_method     = "POST"
-  payload_format_version = "2.0"
-}
+# resource "aws_apigatewayv2_integration" "get_results_from_dynamodb_lambda_integration" {
+#   api_id                 = aws_apigatewayv2_api.tetris_api.id
+#   integration_uri        = aws_lambda_function.get_result_from_dynamodb_function.invoke_arn
+#   integration_type       = "AWS_PROXY"
+#   integration_method     = "POST"
+#   payload_format_version = "2.0"
+# }
 
-resource "aws_apigatewayv2_integration" "entry_competition_lambda_integration" {
-  api_id                 = aws_apigatewayv2_api.tetris_api.id
-  integration_uri        = aws_lambda_function.entry_competition_function.invoke_arn
-  integration_type       = "AWS_PROXY"
-  integration_method     = "POST"
-  payload_format_version = "2.0"
-}
+# resource "aws_apigatewayv2_integration" "entry_competition_lambda_integration" {
+#   api_id                 = aws_apigatewayv2_api.tetris_api.id
+#   integration_uri        = aws_lambda_function.entry_competition_function.invoke_arn
+#   integration_type       = "AWS_PROXY"
+#   integration_method     = "POST"
+#   payload_format_version = "2.0"
+# }
 
-resource "aws_apigatewayv2_integration" "get_entries_lambda_integration" {
-  api_id                 = aws_apigatewayv2_api.tetris_api.id
-  integration_uri        = aws_lambda_function.get_competition_entries_function.invoke_arn
-  integration_type       = "AWS_PROXY"
-  integration_method     = "POST"
-  payload_format_version = "2.0"
-}
+# resource "aws_apigatewayv2_integration" "get_entries_lambda_integration" {
+#   api_id                 = aws_apigatewayv2_api.tetris_api.id
+#   integration_uri        = aws_lambda_function.get_competition_entries_function.invoke_arn
+#   integration_type       = "AWS_PROXY"
+#   integration_method     = "POST"
+#   payload_format_version = "2.0"
+# }
 
-resource "aws_apigatewayv2_route" "send_message_lambda" {
-  api_id    = aws_apigatewayv2_api.tetris_api.id
-  route_key = "POST /score_evaluation"
-  target    = "integrations/${aws_apigatewayv2_integration.send_message_to_sqs_lambda_integration.id}"
-}
+# resource "aws_apigatewayv2_route" "send_message_lambda" {
+#   api_id    = aws_apigatewayv2_api.tetris_api.id
+#   route_key = "POST /score_evaluation"
+#   target    = "integrations/${aws_apigatewayv2_integration.send_message_to_sqs_lambda_integration.id}"
+# }
 
-resource "aws_apigatewayv2_route" "get_results_from_dynamodb_route" {
-  api_id    = aws_apigatewayv2_api.tetris_api.id
-  route_key = "GET /results"
-  target    = "integrations/${aws_apigatewayv2_integration.get_results_from_dynamodb_lambda_integration.id}"
-}
+# resource "aws_apigatewayv2_route" "get_results_from_dynamodb_route" {
+#   api_id    = aws_apigatewayv2_api.tetris_api.id
+#   route_key = "GET /results"
+#   target    = "integrations/${aws_apigatewayv2_integration.get_results_from_dynamodb_lambda_integration.id}"
+# }
 
-resource "aws_apigatewayv2_route" "entry_competition_lambda" {
-  api_id    = aws_apigatewayv2_api.tetris_api.id
-  route_key = "POST /entry"
-  target    = "integrations/${aws_apigatewayv2_integration.entry_competition_lambda_integration.id}"
-}
+# resource "aws_apigatewayv2_route" "entry_competition_lambda" {
+#   api_id    = aws_apigatewayv2_api.tetris_api.id
+#   route_key = "POST /entry"
+#   target    = "integrations/${aws_apigatewayv2_integration.entry_competition_lambda_integration.id}"
+# }
 
-resource "aws_apigatewayv2_route" "get_competition_entries_lambda" {
-  api_id    = aws_apigatewayv2_api.tetris_api.id
-  route_key = "GET /entries"
-  target    = "integrations/${aws_apigatewayv2_integration.get_entries_lambda_integration.id}"
-}
+# resource "aws_apigatewayv2_route" "get_competition_entries_lambda" {
+#   api_id    = aws_apigatewayv2_api.tetris_api.id
+#   route_key = "GET /entries"
+#   target    = "integrations/${aws_apigatewayv2_integration.get_entries_lambda_integration.id}"
+# }

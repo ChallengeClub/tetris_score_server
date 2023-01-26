38fe5234ea59108cb3f936a0df3d7848255f19dc
EndoNrak <endo.noriaki.64m@kyoto-u.jp>
2023-01-26 22:58:03 +0900

    delete output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "api_endpoint_url" {
-  value     = aws_apigatewayv2_stage.tetris_api_stage.invoke_url
-  sensitive = true
-}
+# output "api_endpoint_url" {
+#   value     = aws_apigatewayv2_stage.tetris_api_stage.invoke_url
+#   sensitive = true
+# }
 
 output "sqs_url" {
   value     = data.aws_sqs_queue.score_evaluation_queue_data.url

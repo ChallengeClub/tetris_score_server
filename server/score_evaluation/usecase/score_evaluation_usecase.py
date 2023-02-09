e637fc94bbe2ac5f3bd8a1727cd521ab6520e48f
EndoNrak <spheniscushumboldtievangelion@gmail.com>
2023-02-09 21:19:02 +0900

    add update_started_at method in dynamodb client

--- server/score_evaluation/usecase/score_evaluation_usecase.py
+++ server/score_evaluation/usecase/score_evaluation_usecase.py
@@ -21,6 +21,11 @@ class ScoreEvaluationUsecase:
             eval_app = ScoreEvaluationApplication(_eval)
             print("start evaluation:\t", _eval)
             _eval.started_at = int(time())
+            _eval.status = "evaluating"
+            res = self.evaluation_dynamodb_repository_interface.update_started_at(_eval)
+            if res['ResponseMetadata']['HTTPStatusCode'] != 200:
+                print("failed update to dynamodb:\t", res)
+
             _eval = eval_app.evaluate(log_folder="/server/log")
             _eval.ended_at = int(time())
             print("finish evaluation:\t", _eval)

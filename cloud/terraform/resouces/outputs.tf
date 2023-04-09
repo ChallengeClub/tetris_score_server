cdf97a4a23a444738cdca9ffb32128d50c4a7a00
EndoNrak <endo.noriaki.64m@kyoto-u.jp>
2023-04-09 18:56:58 +0900

    remove terraform on lambda and apigateway

--- cloud/terraform/resouces/outputs.tf
+++ cloud/terraform/resouces/outputs.tf
@@ -1,8 +1,3 @@
-output "api_endpoint_url" {
-  value     = aws_apigatewayv2_stage.tetris_api_stage.invoke_url
-  sensitive = true
-}
-
 output "sqs_url" {
   value     = data.aws_sqs_queue.score_evaluation_queue_data.url
   sensitive = true
